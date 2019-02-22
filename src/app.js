64e20471c9bf04e0668f24fe1ee92288d0908198
Jacob Bare <jacob@limit0.io>
2019-02-22 12:33:45 -0600

    Create new default layout

--- src/app.js
+++ src/app.js
@@ -11,7 +11,8 @@ app.use(helmet());
 
 registerHelpers(hbs);
 app.engine('hbs', hbs.express4({
-  defaultLayout: `${__dirname}/views/layouts/default`,
+  contentHelperName: 'content-for',
+  defaultLayout: `${__dirname}/views/_document`,
   partialsDir: `${__dirname}/views/partials`,
 }));
 app.set('view engine', 'hbs');
