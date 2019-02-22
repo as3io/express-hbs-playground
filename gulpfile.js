cf44b1e27e9a98c8deb83754dfbb7bdf3a75cda7
Jacob Bare <jacob@limit0.io>
2019-02-22 10:23:19 -0600

    Add livereload server and event emitter

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,26 @@ const {
 const eslint = require('gulp-eslint');
 const cache = require('gulp-cached');
 const { spawn } = require('child_process');
+const http = require('http');
+const socketio = require('socket.io');
 
 const { log } = console;
+
+const { LIVERELOAD_PORT } = process.env;
+const server = http.createServer();
+const io = socketio(server, { path: '/_lr' });
+server.listen(LIVERELOAD_PORT);
+
+io.on('connection', () => log('> Web browser connected to livereload.'));
 
 let node;
 
 const serve = async () => {
   if (node) node.kill();
-  node = await spawn('node', ['src/index.js'], { stdio: 'inherit' });
+  node = await spawn('node', ['src/index.js'], { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] });
+  node.on('message', (msg) => {
+    if (msg === 'ready') io.emit('process-ready');
+  });
   node.on('close', (code, signal) => {
     log(`> Node subprocess (via Gulp) exited with code ${code} signal ${signal}`);
   });
