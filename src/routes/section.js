9fc8a1354ca61b9f58f184be3f1726aabc8799d9
Jacob Bare <jacob@limit0.io>
2019-02-22 12:34:00 -0600

    Loop over contrived delay helpers

--- src/routes/section.js
+++ src/routes/section.js
@@ -1,12 +1,26 @@
 const { Router } = require('express');
+const rand = require('../utils/random-range');
 
 const router = Router();
 
 router.get('/:alias', (req, res) => {
   const { alias } = req.params;
+
+  const times = [
+    rand(100, 350),
+    rand(100, 350),
+    rand(100, 350),
+    rand(100, 350),
+  ];
+  const totalTime = times.reduce((n, v) => n + v, 0);
+  const expectedTime = Math.max(...times) + 20;
+
   res.render('section', {
     title: 'Some title',
     alias,
+    times,
+    totalTime,
+    expectedTime,
   });
 });
 
